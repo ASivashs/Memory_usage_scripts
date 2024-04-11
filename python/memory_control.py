b497938c8cd93323fc83cc54878d8fcda0893530
Anton Sivashka <anton.sivashko@gmail.com>
2024-04-11 12:25:29 +0300

    Update sleep time

--- python/memory_control.py
+++ python/memory_control.py
@@ -10,10 +10,7 @@ import logging
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
-    handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler("memory_control.log")
-    ]
+    handlers=[logging.StreamHandler(), logging.FileHandler("memory_control.log")],
 )
 
 logger = logging.getLogger(__name__)
@@ -23,12 +20,13 @@ def get_memory_usage() -> dict:
     """
     Retrieves the memory usage of the system.
 
-    :return: (dict) The memory usage information with percentage of 
+    :return: (dict) The memory usage information with percentage of
     the system used memory.
     """
     if platform.startswith("linux"):
-        total, used, free, shared, cache, available = \
-            check_output(["free", "-m"]).decode("utf-8").split('\n')[1].split()[1:7]
+        total, used, free, shared, cache, available = (
+            check_output(["free", "-m"]).decode("utf-8").split("\n")[1].split()[1:7]
+        )
         usage_percentage = round((int(used) / int(total) * 100), 2)
         response = {
             "total": total,
@@ -43,34 +41,34 @@ def get_memory_usage() -> dict:
 
 def send_request(message: dict, url: str) -> int | None:
     """
-    Sends an HTTP POST request to a specified URL with information about 
+    Sends an HTTP POST request to a specified URL with information about
     the memory usage.
-    
+
     :param message: (dict) Request message.
     :param url: (str) Specify alarm http request to api.
-    :return: (int or None) Return status code of http response or 
+    :return: (int or None) Return status code of http response or
     nothing if success.
     """
     timeout = 5
-    
+
     try:
         response = requests.post(url, timeout=timeout, json=message)
         response.raise_for_status()
         logger.info("Request sent successfully. %s", message)
-        
+
     except requests.Timeout as err:
         logger.error("Timeout: %s.", err)
-        
+
     except requests.exceptions.RequestException as err:
         logger.error("Request exception: %s.", err)
-        
+
     except requests.exceptions.HTTPError as err:
         status_code = err.response.status_code
         logger.error("Http exception: %s. Status code: %i.", err, status_code)
-        
+
     except Exception as err:
         logger.error("Exception: %s.", err)
-    
+
     else:
         return response.status_code
     return
@@ -78,19 +76,19 @@ def send_request(message: dict, url: str) -> int | None:
 
 @click.command()
 @click.option(
-    "-m", 
-    "--memory-usage", 
-    type=float, 
-    default=80., 
-    help="System memory usage in percent."
-    )
+    "-m",
+    "--memory-usage",
+    type=float,
+    default=80.0,
+    help="System memory usage in percent.",
+)
 @click.option(
-    "-r", 
-    "--request-url", 
-    type=str, 
-    default="http://127.0.0.1:8080/reports/", 
-    help="URL of http request to api."
-    )
+    "-r",
+    "--request-url",
+    type=str,
+    default="http://127.0.0.1:8080/reports/",
+    help="URL of http request to api.",
+)
 def check(memory_usage: int, request_url: str):
     """
     Monitor system memory consumption every second and generate HTTP POST
@@ -101,9 +99,8 @@ def check(memory_usage: int, request_url: str):
         current_memory_usage = current_memory_info["used_percentage"]
         if current_memory_usage >= memory_usage:
             send_request(current_memory_info, request_url)
-            sleep(1)
-            
+            sleep(3)
+
 
 if __name__ == "__main__":
     check()
-    
